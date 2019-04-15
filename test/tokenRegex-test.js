0e4b6cfb1faf66270a88cc5dec51951d5af07b7f
Dibad <dibadgithub@gmail.com>
2019-04-15 11:45:20 +0100

    Tests for string regex. Removed 'line' attribute from returned token

--- test/tokenRegex-test.js
+++ test/tokenRegex-test.js
@@ -3,34 +3,41 @@ const should = require("should")
 
 describe("Regex for LP token", () => {
   it("should recognize ( as valid a token", () => {
-    const expected = { type: "LP", value: "(", start: 1, end: 1, line: 1 };
+    const expected = { type: "LP", value: "(", start: 1, end: 1};
     tokenRegex.LP.reset().exec("(").should.be.eql(expected);
   });
 
   it("should treat { as a synonym for (", () => {
-    const expected = { type: "LP", value: "{", start: 1, end: 1, line: 1 };
+    const expected = { type: "LP", value: "{", start: 1, end: 1};
     tokenRegex.LP.reset().exec("{").should.be.eql(expected);
   });
 
   it("should treat [ as a synonym for (", () => {
-    const expected = { type: "LP", value: "[", start: 1, end: 1, line: 1 };
+    const expected = { type: "LP", value: "[", start: 1, end: 1};
     tokenRegex.LP.reset().exec("[").should.be.eql(expected);
   });
 });
 
 describe("Regex for RP token", () => {
   it("should recognize ) as valid a token", () => {
-    const expected = { type: "RP", value: ")", start: 1, end: 1, line: 1 };
+    const expected = { type: "RP", value: ")", start: 1, end: 1};
     tokenRegex.RP.reset().exec(")").should.be.eql(expected);
   });
 
   it("should treat } as a synonym for )", () => {
-    const expected = { type: "RP", value: "}", start: 1, end: 1, line: 1 };
+    const expected = { type: "RP", value: "}", start: 1, end: 1};
     tokenRegex.RP.reset().exec("}").should.be.eql(expected);
   });
 
   it("should treat ] as a synonym for )", () => {
-    const expected = { type: "RP", value: "]", start: 1, end: 1, line: 1 };
+    const expected = { type: "RP", value: "]", start: 1, end: 1};
     tokenRegex.RP.reset().exec("]").should.be.eql(expected);
+  });
+});
+
+describe("Regex for STRING token", () => {
+  it("should recognize anything between \" \" as valid a token", () => {
+    const expected = { type: "STRING", value: "hello world", start: 3, end: 13};
+    tokenRegex.STRING.reset().exec("\"hello world\"").should.be.eql(expected);
   });
 });
