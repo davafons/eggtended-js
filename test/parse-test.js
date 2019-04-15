c1621c86abd7c61c9c0d2d90a5ed9de14a2eb9e6
Dibad <dibadgithub@gmail.com>
2019-04-15 15:47:02 +0100

    Parsing correctly an Egg programgit add .!

--- test/parse-test.js
+++ test/parse-test.js
@@ -1,8 +1,17 @@
 const tokenRegex = require("../lib/tokenRegex.js");
+const fs = require('fs');
 const should = require("should")
+
+const parse = require("../lib/parse.js");
+const parser = new parse.Parser();
 
 describe("Test program 'examples/one.egg'", () => {
   it("should be parsed correctly", () => {
+    const raw_data = fs.readFileSync('examples/one.evm');
+    const expected_evm = JSON.parse(raw_data);
+
+    parser.parseFromFile("examples/one.egg").should.be.eql(expected_evm);
+
     // TODO: Define test
   });
 });
