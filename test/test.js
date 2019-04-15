d323ddca7da3c337c5d78ec4ee58ed83185240a9
Dibad <dibadgithub@gmail.com>
2019-04-15 11:41:29 +0100

    Removed unused tests. Added tests for RP regex

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const should = require("should");
-
-
-describe("example-test", function() {
-    it("should be true", function() {
-        true.should.eql(true);
-    });
-});
-
--- test/tokenRegex-test.js
+++ test/tokenRegex-test.js
@@ -1,4 +1,5 @@
 const tokenRegex = require("../lib/tokenRegex.js");
+const should = require("should")
 
 describe("Regex for LP token", () => {
   it("should recognize ( as valid a token", () => {
@@ -14,5 +15,22 @@ describe("Regex for LP token", () => {
   it("should treat [ as a synonym for (", () => {
     const expected = { type: "LP", value: "[", start: 1, end: 1, line: 1 };
     tokenRegex.LP.reset().exec("[").should.be.eql(expected);
+  });
+});
+
+describe("Regex for RP token", () => {
+  it("should recognize ) as valid a token", () => {
+    const expected = { type: "RP", value: ")", start: 1, end: 1, line: 1 };
+    tokenRegex.RP.reset().exec(")").should.be.eql(expected);
+  });
+
+  it("should treat } as a synonym for )", () => {
+    const expected = { type: "RP", value: "}", start: 1, end: 1, line: 1 };
+    tokenRegex.RP.reset().exec("}").should.be.eql(expected);
+  });
+
+  it("should treat ] as a synonym for )", () => {
+    const expected = { type: "RP", value: "]", start: 1, end: 1, line: 1 };
+    tokenRegex.RP.reset().exec("]").should.be.eql(expected);
   });
 });
