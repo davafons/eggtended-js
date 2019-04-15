c9e6aec49aebc24058a86e213305010634904222
Dibad <dibadgithub@gmail.com>
2019-04-15 15:14:38 +0100

    Example parsing a real .egg program. Bug with words and values

---
+++ test/parse-test.js
@@ -0,0 +1,9 @@
+const tokenRegex = require("../lib/tokenRegex.js");
+const should = require("should")
+
+describe("Test program 'examples/one.egg'", () => {
+  it("should be parsed correctly", () => {
+    // TODO: Define test
+  });
+});
+
