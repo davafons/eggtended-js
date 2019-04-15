002f17d38adbfa6981e324ec6b943f7aa63bffe3
Dibad <dibadgithub@gmail.com>
2019-04-15 18:27:38 +0100

    Added binary file for using the compiler

---
+++ bin/eggc.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const {Parser} = require('../lib/parse.js');
+const fs = require('fs');
+
+const file = process.argv.slice(2).shift();
+if (file && file.length > 0) {
+  let parser = new Parser();
+  let tree = parser.parseFromFile(file);
+
+  const json = JSON.stringify(tree, null, "  ");
+  fs.writeFileSync(file+".evm", json);
+
+  console.log(json);
+}
