779f18cd4a7f6084c28d9b5b89e91f3f63bb8df5
Dibad <dibadgithub@gmail.com>
2019-04-16 19:29:52 +0100

    Added test for 'examples/reto.egg'

--- test/examples-test.js
+++ test/examples-test.js
@@ -7,8 +7,7 @@ const {Eggvm} = require('../lib/eggvm.js');
 const parser = new Parser();
 const eggvm = new Eggvm();
 
-describe('Test program \'examples/one.egg\'', () => {
-
+describe('Testing programs from \'examples/\' folder', () => {
   beforeEach(() => {
     this.logSpy = sinon.spy(console, 'log');
   });
@@ -17,61 +16,80 @@ describe('Test program \'examples/one.egg\'', () => {
     this.logSpy.restore();
   });
 
-  it('should be parsed correctly', () => {
-    const rawData = fs.readFileSync('examples/one.egg.evm');
-    const expected = JSON.parse(rawData);
+  describe('Test \'examples/one.egg\'', () => {
 
-    parser.parseFromFile('examples/one.egg').should.be.eql(expected);
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/one.egg.evm');
+      const expected = JSON.parse(rawData);
+
+      parser.parseFromFile('examples/one.egg').should.be.eql(expected);
+    });
+
+    it('should return the expected output after executing', () => {
+      const expected = 50;
+      eggvm.runFromEVM('examples/one.egg.evm').should.be.eql(expected);
+      this.logSpy.calledWith(50).should.be.true();
+    });
   });
 
-  it('should return the expected output after executing', () => {
-    const expected = 50;
-    eggvm.runFromEVM('examples/one.egg.evm').should.be.eql(expected);
-    this.logSpy.calledWith(50).should.be.true();
-  });
-});
+  describe('Test \'examples/scope.egg\'', () => {
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/scope.egg.evm');
+      const expected = JSON.parse(rawData);
 
-describe('Test program \'examples/scope.egg\'', () => {
-  it('should be parsed correctly', () => {
-    const rawData = fs.readFileSync('examples/scope.egg.evm');
-    const expected = JSON.parse(rawData);
-
-    parser.parseFromFile('examples/scope.egg').should.be.eql(expected);
-  });
-});
-
-describe('Test program \'examples/scope-err.egg\'', () => {
-  it('should be parsed correctly', () => {
-    const rawData = fs.readFileSync('examples/scope-err.egg.evm');
-    const expected = JSON.parse(rawData);
-
-    parser.parseFromFile('examples/scope-err.egg').should.be.eql(expected);
+      parser.parseFromFile('examples/scope.egg').should.be.eql(expected);
+    });
   });
 
-  it('should return the expected output after executing', () => {
-    should.throws( () => { eggvm.runFromEVM('examples/scope-err.egg.evm')}, ReferenceError );
+  describe('Test \'examples/scope-err.egg\'', () => {
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/scope-err.egg.evm');
+      const expected = JSON.parse(rawData);
+
+      parser.parseFromFile('examples/scope-err.egg').should.be.eql(expected);
+    });
+
+    it('should return the expected output after executing', () => {
+      should.throws( () => { eggvm.runFromEVM('examples/scope-err.egg.evm')}, ReferenceError );
+    });
   });
-});
 
-describe('Test program \'examples/string.egg\'', () => {
-  it('should be parsed correctly', () => {
-    const rawData = fs.readFileSync('examples/string.egg.evm');
-    const expected = JSON.parse(rawData);
+  describe('Test \'examples/string.egg\'', () => {
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/string.egg.evm');
+      const expected = JSON.parse(rawData);
 
-    parser.parseFromFile('examples/string.egg').should.be.eql(expected);
+      parser.parseFromFile('examples/string.egg').should.be.eql(expected);
+    });
+
+    it('should return the expected output after executing', () => {
+      eggvm.runFromEVM('examples/string.egg.evm');
+      this.logSpy.calledWith('s').should.be.true();
+    });
   });
 
-  it('should return the expected output after executing', () => {
-    const expected = 's';
-    eggvm.runFromEVM('examples/string.egg.evm').should.be.eql(expected);
-  });
-});
+  describe('Test \'examples/sum.egg\'', () => {
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/sum.egg.evm');
+      const expected = JSON.parse(rawData);
 
-describe('Test program \'examples/sum.egg\'', () => {
-  it('should be parsed correctly', () => {
-    const rawData = fs.readFileSync('examples/sum.egg.evm');
-    const expected = JSON.parse(rawData);
-
-    parser.parseFromFile('examples/sum.egg').should.be.eql(expected);
+      parser.parseFromFile('examples/sum.egg').should.be.eql(expected);
+    });
   });
+
+  describe('Test \'examples/reto.egg\'', () => {
+    it('should be parsed correctly', () => {
+      const rawData = fs.readFileSync('examples/reto.egg.evm');
+      const expected = JSON.parse(rawData);
+
+      parser.parseFromFile('examples/reto.egg').should.be.eql(expected);
+    });
+
+    it('should return the expected output after executing', () => {
+      eggvm.runFromEVM('examples/reto.egg.evm');
+      this.logSpy.calledWith('sum(array[1, 2, 3]) := 6').should.be.true();
+    });
+  });
+
+
 });
