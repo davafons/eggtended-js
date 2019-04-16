601d636dde4a1ee6efb8a81fb49cae0658f496be
Dibad <dibadgithub@gmail.com>
2019-04-16 11:07:49 +0100

    Restructured tests. Added define specialForm

--- test/eggvm-test.js
+++ test/eggvm-test.js
@@ -3,21 +3,45 @@ const should = require("should")
 const {Evaluator} = require("../lib/eggvm.js");
 
 describe("Test SpecialForms", () => {
-  let ev;
+  const ev = new Evaluator();
 
-  beforeEach(() => {
-    ev = new Evaluator();
+  describe("do(...)", () => {
+    it("should return the argument evaluated", () => {
+      ev.run("do(3)").should.be.eql(3);
+      ev.run("do(\"hola\")").should.be.eql("hola");
+    });
+
+    it("should return the result of the evaluated expression", () => {
+      ev.run("do(if(true, 3, 4))").should.be.eql(3);
+    });
   });
 
+  describe("if(cond, v1, v2)", () => {
+    it("should return the first argument if evaluates to true", () => {
+      ev.run("if(true, 3, 4)").should.be.eql(3);
+    });
 
-  it("should evaluate do(...)", () => {
-    ev.run("do(3)").should.be.eql(3);
+    it("should return the second argument if evaluates to false", () => {
+      ev.run("if(false, 3, 4)").should.be.eql(4);
+    });
 
-    ev.run("do(\"hola\")").should.be.eql("hola");
+    it("should throw SyntaxError if not passing exactly 3 arguments", () => {
+      should.throws(() => { ev.run("if(true, 3)"); }, SyntaxError);
+      should.throws(() => { ev.run("if(true, 3, 4, 5)"); }, SyntaxError);
+    });
   });
 
-  it("should evaluate if(exp, r1, r2)", () => {
-    ev.run("if(true, 3, 4)").should.be.eql(3);
-    ev.run("if(false, 3, 4)").should.be.eql(4);
+  describe("[def|define](word, value)", () => {
+    it("should throw if trying to acces an undefined word", () => {
+      should.throws(() => { ev.run("do(x)"); }, ReferenceError);
+    });
+
+    it("should return the value of the defined word", () => {
+      ev.run("do(define(x, 3), x)").should.be.eql(3);
+    });
+
+    it("should treat 'def' and 'define' as synonyms", () => {
+      ev.run("do(def(x, 3), x)").should.be.eql(3);
+    });
   });
 });
